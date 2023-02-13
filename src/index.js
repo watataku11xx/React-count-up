ce13318f3e21d28ff3f05c9862694062057d3876
watataku11xx <watataku131122@outlook.jp>
2023-02-14 01:50:46 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,83 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './styles.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+(() => {
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    function Counter(props){
+        return(
+            <li style={{backgroundColor:props.counter.color}} onClick={() => 
+            props.countUp(props.counter)}>
+                {props.counter.id}:{props.counter.count}
+            </li>
+        );
+    }
+
+    function CounterList(props){
+        const counters =props.counters.map(counter => {
+            return (
+                <Counter
+                counter={counter}
+                key={counter.id}
+                countUp={props.countUp}
+                />
+            );
+        });
+        return (
+            <ul>
+                {counters}
+            </ul>
+        );
+    }
+
+    class App extends React.Component{
+        constructor(){
+            super();
+            this.state ={
+                counters: [
+                    {id: 'A', count: 0, color: 'tomato'},
+                    {id: 'B', count: 0, color: 'skyblue'},
+                    {id: 'C', count: 0, color: 'limegreen'},
+                    {id: 'D', count: 0, color: 'gray'},
+                    {id: 'E', count: 0, color: 'pink'}
+                ],
+                total: 0,
+            };
+            this.countUp = this.countUp.bind(this);
+        }
+
+        countUp(counter){
+            this.setState(prevState => {
+                const counters = prevState.counters.map(counter => {
+                    return {id: counter.id, count: counter.count, color: counter.color};
+                });
+                const pos = counters.map(counter => {
+                    return counter.id;
+                }).indexOf(counter.id);
+                counters[pos].count++;
+                return {
+                    counters: counters,
+                    total: prevState.total + 1
+                };
+            });
+        }
+
+        render(){
+            return (
+                <div className="container">
+                    <CounterList counters={this.state.counters}
+                    countUp={this.countUp}/>
+                    <div>TOTAL INVENTORY: {this.state.total}</div>
+                </div>
+            );
+        }
+    }
+
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+    root.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+        document.getElementById('root')
+    );
+})();
